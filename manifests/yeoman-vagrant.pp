1f2d8a6fa9f7e248c3f88821d5995ee176e90b0f
Sebastian Schulze <github.com@bascht.com>
2013-03-28 22:56:22 +0100

    Split Manifest and resolve deps via classes.

--- manifests/yeoman-vagrant.pp
+++ manifests/yeoman-vagrant.pp
@@ -1,39 +1,44 @@
+class yeoman_base {
+  exec {"update":
+    command => "/usr/bin/apt-get update"
+  }
 
-exec {"update":
-  command => "apt-get update",
-  user => root,
+  package { "pre-deps":
+    name => [
+      "python-software-properties",
+    ],
+    require => Exec["update"],
+  }
+
+  exec {"nodejs-repo":
+    command => "/usr/bin/apt-add-repository ppa:chris-lea/node.js && apt-get update",
+    cwd => "/opt/yeoman-vagrant",
+    user => root,
+    creates => "/etc/apt/sources.list.d/chris-lea-node_js-precise.list",
+    require => Package["pre-deps"]
+  }
+
+  package { "deps":
+    name => ["nodejs"],
+    ensure => present,
+    require => Exec["nodejs-repo"]
+  }
 }
 
-package { "pre-deps":
-  name => [
-    "python-software-properties",
-  ],
-  require => Exec["update"],
+class yeoman_packages {
+  exec { "npm-install-g":
+    cwd => "/opt/yeoman-vagrant",
+    command => "/usr/bin/npm install -g grunt-cli",
+    user => root,
+    require => Class['yeoman_base']
+  }
+
+  exec { "npm-install":
+    command => "/usr/bin/npm install",
+    cwd => "/opt/yeoman-vagrant",
+    require => Exec["npm-install-g"]
+  }
 }
 
-exec {"nodejs-repo":
-  command => "apt-add-repository ppa:chris-lea/node.js && apt-get update",
-  cwd => "/opt/yeoman-vagrant",
-  user => root,
-  creates => "/etc/apt/sources.list.d/chris-lea-node_js-precise.list",
-  require => Package["pre-deps"],
-}
-
-package { "deps":
-  name => ["nodejs"],
-  ensure => present,
-  require => Exec["nodejs-repo"],
-}
-
-exec { "npm-install-g":
-  cwd => "/opt/yeoman-vagrant",
-  command => "npm install -g grunt-cli",
-  user => root,
-  require => Package["deps"],
-}
-
-exec { "npm-install":
-  command => "npm install",
-  cwd => "/opt/yeoman-vagrant",
-  require => Exec["npm-install-g"],
-}
+include yeoman_base
+include yeoman_packages
